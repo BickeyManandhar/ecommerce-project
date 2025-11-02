53dcfe910b71909f887fe9966620d59c7106d1fe
BickeyManandhar <manandharbickey@gmail.com>
2025-11-02 16:10:46 -0500

    BICKEY-11-02-2025 added some more styling using css bootstrap and fortawesome and html changes

--- backend/spring-boot-ecommerce/src/main/java/com/bickey/spring_boot_ecommerce/controller/ProductController.java
+++ backend/spring-boot-ecommerce/src/main/java/com/bickey/spring_boot_ecommerce/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.bickey.spring_boot_ecommerce.controller;
 
 import com.bickey.spring_boot_ecommerce.dto.ProductDto;
 import com.bickey.spring_boot_ecommerce.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +13,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/product")
 @CrossOrigin("http://localhost:4200")
+@Slf4j
 public class ProductController {
+
+    private static int callCount = 0;
 
     private final ProductService productService;
 
@@ -22,11 +26,15 @@ public class ProductController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ProductDto>> getAllProduct(){
+        callCount++;
+        log.info("Number of calls made on getAllProduct(): "+ callCount);
         return new ResponseEntity<>(productService.getAllProduct(), HttpStatus.OK);
     }
 
     @GetMapping(value="{id}",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ProductDto> getProductById(@PathVariable Long id){
+        callCount++;
+        log.info("Number of calls made on getProductById(): "+ callCount);
         return new ResponseEntity<>(productService.getProductById(id), HttpStatus.OK);
     }
 }
